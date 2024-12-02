d6af04eeeac537467f2c4192b4f1d2636967476d
FranMulti1200 <fbautista848stf@gmail.com>
2024-12-02 13:43:11 +0100

    Modification exercise with using sql database

--- codingExercise/Scraping-data-temperature/main_file.py
+++ codingExercise/Scraping-data-temperature/main_file.py
@@ -1,6 +1,6 @@
 import requests
 import selectorlib
-import sqlite3
+
 
 from datetime import datetime
 
@@ -10,8 +10,7 @@ URL = "http://programmer100.pythonanywhere.com/"
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
-# Establish a connection
-connection = sqlite3.connect("temperature.db")
+
 
 def scrape(url):
     """Scrape the page source from the URL"""
@@ -28,10 +27,10 @@ def extract(source):
 
 def store(extracted):
     now = datetime.now().strftime("%y-%m-%d-%H-%M-%S")
-    extracted = int(extracted)
-    cursor = connection.cursor()
-    cursor.execute("INSERT INTO temp VALUES(?,?)", (now, extracted))
-    connection.commit()
+    with open("data.txt", "a") as file:
+        line = f"{now},{extracted}\n"
+        file.write(line)
+
 
 if __name__ == "__main__":
     scraped = scrape(URL)
